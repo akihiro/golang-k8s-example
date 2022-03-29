050545ec16eff5ff835a6202206fd3908b700d11
Hiroaki Mizuguchi <mhiroaki@gmail.com>
2022-03-29 23:16:50 +0900

    Implement a HTTP Server and graceful shutdown

--- main.go
+++ main.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
 func main() {
+	srv := http.Server{
+		Addr: ":2000",
+	}
+	go func() {
+		log.Print(srv.ListenAndServe())
+	}()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+	sig := <-sigCh
+	log.Printf("Recieve signal: %s", sig)
+
+	ctx, stop := context.WithTimeout(context.Background(), time.Second*5)
+	defer stop()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Print(err)
+	}
+	log.Print("finished")
 }
